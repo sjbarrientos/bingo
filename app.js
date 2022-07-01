4bed8211825a723f2ab1fa40c4f9c3a10c1ddefa
sjbarrientos <sjbarrientosb@gmail.com>
2022-07-01 07:06:45 -0500

    ⭐️ feature: first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const Common = require("./utils/common");
+
+let condition = true;
+let cartones = Common.llenarCartones()
+
+
+const main = async () => {
+    do {
+        console.clear()
+
+        Common.imprimir(cartones)
+
+        let option = await Common.readInput();
+        switch (option) {
+            case 'R': {
+                cartones = Common.llenarCartones()
+                break;
+            }
+            case 'S': {
+                condition = false
+                break;
+            }
+
+            default:
+                Common.marcar(cartones, option);
+                break;
+        }
+        console.clear()
+    } while (condition);
+
+}
+.
+main().                     
